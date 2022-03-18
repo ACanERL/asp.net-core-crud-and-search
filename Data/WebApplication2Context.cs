2f1fd86fef81ea20a8c6e0f4acb8baf18ddf2efc
Ahmetcan EROL <71428865+ACanERL@users.noreply.github.com>
2022-03-18 16:19:38 +0300

    asp.net core mvc
    CRUD AND SEARCH

---
+++ Data/WebApplication2Context.cs
@@ -0,0 +1,20 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebApplication2.Models;
+
+namespace WebApplication2.Data
+{
+    public class WebApplication2Context : DbContext
+    {
+        public WebApplication2Context (DbContextOptions<WebApplication2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebApplication2.Models.Movie> Movie { get; set; }
+    }
+}
